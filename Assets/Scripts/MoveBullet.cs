cc0587c3e8d6948971fe0cdbdb7154dcca4f59bc
Zmoore-MCCC <143642207+Zmoore-MCCC@users.noreply.github.com>
2024-10-07 16:33:09 -0400

    10-07 Lecture
    10-07 Lecture

--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -30,4 +30,31 @@ public class MoveBullet : MonoBehaviour
         //Remove the bullet from the screen
         Destroy(this.gameObject);
     }
+
+    public float getBulletDamage()
+    {
+        return bulletDamage;
+    }
+
+    public void setBulletDamage(float d)
+    {
+        bulletDamage = d;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!collision.gameObject.CompareTag("Weapon"))
+        {
+            //destroyBullet();
+        }
+   
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Weapon"))
+        {
+            //destroyBullet();
+        }
+    }
 }
